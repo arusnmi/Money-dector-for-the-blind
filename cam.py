acdd8125cd7cb0a7d4fad435005bfacd85e8e49e
arusnmi <parush.larty@gmail.com>
2025-09-17 11:33:56 +0530

    commit to make claude do it

--- cam.py
+++ cam.py
@@ -1,17 +1,39 @@
-from kivy.uix.camera import Camera
+from kivy.uix.image import Image
+from kivy.graphics.texture import Texture
+from kivy.clock import Clock
+import cv2
+import numpy as np
 
 
 
-def create_camera_widget(resolution=(6400, 4800), play=True):
-    """
-    Create and return a Kivy Camera widget with specified resolution and play state.
+class CameraWidget(Image):
+    def __init__(self, **kwargs):
+        super(CameraWidget, self).__init__(**kwargs)
+        self.capture = cv2.VideoCapture(0)
+        self.frame = None
 
-    :param resolution: A tuple specifying the (width, height) of the camera feed.
-    :param play: A boolean indicating whether the camera should start playing immediately.
-    :return: An instance of the Camera widget.
-    """
-    camera = Camera(resolution=resolution, play=play)
+    def start(self):
+        Clock.schedule_interval(self.update, 1.0/30.0)
+
+    def update(self, dt):
+        ret, frame = self.capture.read()
+        if ret:
+            self.frame = frame
+            # Convert to texture for display
+            buf = cv2.flip(frame, 0).tostring()
+            texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            self.texture = texture
+
+    def get_frame(self):
+        return self.frame
+
+
+
+def create_camera_widget():
+    camera = CameraWidget()
+    camera.start()
     return camera
-    
+
 
 
